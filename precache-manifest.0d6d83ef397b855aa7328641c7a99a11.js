03dae9318b10700e175f92df7099c7041c691972
Mickey Gerassi <mickey.gerassi@gmail.com>
2019-07-30 11:25:40 +0300

    Updates

---
+++ precache-manifest.0d6d83ef397b855aa7328641c7a99a11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1a7a4aa3744d724deec7721ee1dbd5",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "06fd69dc1d75fd6b1b72",
+    "url": "/to-do-list/static/css/main.5d2cc805.chunk.css"
+  },
+  {
+    "revision": "b42c7be64b9cb9ad990d",
+    "url": "/to-do-list/static/js/2.49fd438d.chunk.js"
+  },
+  {
+    "revision": "06fd69dc1d75fd6b1b72",
+    "url": "/to-do-list/static/js/main.76599472.chunk.js"
+  },
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  }
+]);
